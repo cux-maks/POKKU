7708c3cbd49f144a9f57857bbe04ac5a1f1b000d
cux-maks <volcano020716@gmail.com>
2024-01-04 10:09:23 +0900

    app.js 수정 user_info_github.js 파일 생성

---
+++ server/routes/user_info_github.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+
+const maria = require('./maria');
+
+// create user_info_github
+router.post('/', function(req, res) {
+    maria.query(``, function(err, rows, fields) {
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log("err: " + err);
+            res.send(err);
+        }
+    });
+});
+
+// select all user_info_github
+router.get('/', function(req, res) {
+    maria.query('', function(err, rows, fields) {
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log("err: " + err);
+            res.send(err);
+        }
+    });
+});
+
+// select by id user_info_github
+router.get('/', function(req, res) {
+    maria.query(``, function(err, rows, fields) {
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log("err: " + err);
+            res.send(err);
+        }
+    });
+});
+
+// update by id user_info_github
+router.put('/', function(req, res) {
+    maria.query(``, function(err, rows, fields) {
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log("err: " + err);
+            res.send(err);
+        }
+    });
+});
+
+// delete by id user_info_github
+router.delete('/', function(req, res) {
+    maria.query(``, function(err, rows, fields) {
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log("err: " + err);
+            res.send(err);
+        }
+    });
+});
+
+module.exports = router;
