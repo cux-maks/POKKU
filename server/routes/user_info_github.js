68b1f660ff4a26a0520ca3ca4a33249a6d76e91c
cux-maks <volcano020716@gmail.com>
2024-01-04 10:21:55 +0900

    user_info_github.js update 작성

--- server/routes/user_info_github.js
+++ server/routes/user_info_github.js
@@ -29,7 +29,7 @@ router.get('/', function(req, res) {
 
 // select by id user_info_github
 router.get('/', function(req, res) {
-    maria.query(``, function(err, rows, fields) {
+    maria.query(`select * from user_info_github where user_id = '${req.params.user_id}'`, function(err, rows, fields) {
         if(!err){
             res.send(rows);
         }else{
@@ -41,7 +41,7 @@ router.get('/', function(req, res) {
 
 // update by id user_info_github
 router.put('/', function(req, res) {
-    maria.query(``, function(err, rows, fields) {
+    maria.query(`update user_info_github set user_id = '${req.body.user_id}', user_github = '${req.body.user_github}' where user_id = '${req.body.user_id}'`, function(err, rows, fields) {
         if(!err){
             res.send(rows);
         }else{
